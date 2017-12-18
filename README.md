1d6c962c88eacac39c4520e666a93451d8e058fd
kenstars <kenstars@github.com>
2017-12-18 12:19:43 +0530

    added git ignore , modified README and config.json and modified import_sql to add parameters for adding all the tables

--- README.md
+++ README.md
@@ -6,6 +6,22 @@ Give absolute path of the directory containing config.json in the config.json fi
 1. Load the data into the SQL database.
     1.1 Install MySQL if it does not exists.
     1.2 Enter MySQL config information in config.json for keys mysql_user and mysql_password
-    1.3 Run import_sql.py from ./Mindstix/ using the command :
+    1.3 You will need to install MySQLDB python package and dependencies:
+        Ubuntu 14.04:
+            1. sudo apt-get install python-pip python-dev libmysqlclient-dev
+            2. pip install MySQL-python
+    1.4 Run import_sql.py from ./Mindstix/ using the command :
             python db/import_sql.py
     This should create a db and load the csv files into it.
+    This will take a significant amount of time to load.
+    Have patience.
+    1.5 Open mysql and check for the data being correctly dumped
+            select count(*) from item_properties;
+                +----------+
+                | count(*) |
+                +----------+
+                | 20275904 |
+                +----------+
+            1 row in set (4.91 sec)
+
+2. 
--- db/import_sql.py
+++ db/import_sql.py
@@ -17,27 +17,50 @@ def create_db(sql_cursor):
     result = sql_cursor.execute(usesqldb)
     return result
 
-def create_table(sql_cursor):
+def create_table_items(sql_cursor):
     """
     Create all the tables for CSV files in retailrocket
     if they do not exist
     """
 
     create_sql_table = "create table \
-                       IF NOT EXISTS item_properties (timestamp varchar(20), \
-                       item varchar(10), property varchar(15), value integer);"
-    try:
-        result = sql_cursor.execute(create_sql_table)
-    except Warning as warningvariable:
-        print "table exists", warningvariable
-    else:
-        print "in else"
-        os.system(''' mysql -uroot -proot --local_infile=1 retailrocket -e
-                     "LOAD DATA LOCAL INFILE 'db/item_properties_part1.csv'
+                       IF NOT EXISTS item_properties (timestamp double, \
+                       item long, property varchar(15), value varchar(15));"
+    result = sql_cursor.execute(create_sql_table)
+    if not sql_cursor.messages:
+        os.system(''' mysql -uroot -ppassword --local_infile=1 retailrocket -e\
+                     "LOAD DATA LOCAL INFILE 'db/data/item_properties_part1.csv'\
+                     INTO TABLE item_properties FIELDS TERMINATED BY ','"
+                     ''')
+        
+        os.system(''' mysql -uroot -ppassword --local_infile=1 retailrocket -e\
+                     "LOAD DATA LOCAL INFILE 'db/data/item_properties_part2.csv'\
                      INTO TABLE item_properties FIELDS TERMINATED BY ','"
                      ''')
     return result
 
+def create_table_category(sql_cursor):
+    create_sql_table = "create table \
+                       IF NOT EXISTS category_tree (categoryid long, \
+                       parentid long);"
+    result = sql_cursor.execute(create_sql_table)
+    if not sql_cursor.messages:
+        os.system(''' mysql -uroot -ppassword --local_infile=1 retailrocket -e\
+                     "LOAD DATA LOCAL INFILE 'db/data/category_tree.csv'\
+                     INTO TABLE category_tree FIELDS TERMINATED BY ','"
+                     ''')
+    return result
+
+def create_table_events(sql_cursor):
+    create_sql_table = "create table \
+                       IF NOT EXISTS events (timestamp double, \
+                       visitorid long, event varchar(15), itemid long, transactionid long);"
+    result = sql_cursor.execute(create_sql_table)
+    if not sql_cursor.messages:    
+        os.system(''' mysql -uroot -ppassword --local_infile=1 retailrocket -e\
+                     "LOAD DATA LOCAL INFILE 'db/data/events.csv'\
+                     INTO TABLE events FIELDS TERMINATED BY ','"
+                     ''')
 
 if __name__ == '__main__':
     with open("config.json", "r") as config_file:
@@ -46,5 +69,12 @@ if __name__ == '__main__':
                        passwd=str(CONFIG["mysql_password"]))
     CURSOR = DB.cursor()
     RESULT = create_db(CURSOR)
-    RESULT = create_table(CURSOR)
-    print RESULT
+    print "db created.. "
+    print CURSOR.execute("show tables;")
+    RESULT = create_table_items(CURSOR)
+    print CURSOR.execute("show tables;")
+    print "items table created.. "
+    RESULT = create_table_category(CURSOR)
+    print "category table created.. "
+    RESULT = create_table_events(CURSOR)
+    print "events table created.. "
